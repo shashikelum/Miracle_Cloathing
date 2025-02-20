89ba2f4d9f7e5d1528affd5917f3527f9ea79443
Shashi Kelum <shashikelum07@gmail.com>
2025-02-20 23:25:12 +0530

    Intl

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index(){
+        return view('admin.index');
+    }
+
+    public function brands(){
+        $brands = Brand::orderby('id','DESC')->paginate(10);
+        return view('admin.brands', compact('brands'));
+    }
+
+    public function add_brand(){
+
+        return view('admin.brand-add');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Auth::routes();
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard',[UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware(['auth', AuthAdmin::class])->group(function(){
+    Route::get('/admin',[AdminController::class, 'index'])->name('admin.index');
+    Route::get('/admin/brands',[AdminController::class, 'brands'])->name('admin.brands');
+    Route::get('/admin/brand/add',[AdminController::class, 'add_brand'])->name('admin.brand.add');
+
+});
